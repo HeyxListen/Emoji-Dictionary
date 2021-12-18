afa75630d4048bcdc501d9df6df61675c75f21d2
Stephen Coffaro <stephencoffaro@gmail.com>
2021-12-18 11:26:32 -0800

    Files

---
+++ EmojiDictionary3/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary3
+//
+//  Created by csuftitan on 12/1/21.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
---
+++ EmojiDictionary3/EmojiTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary3
+//
+//  Created by csuftitan on 12/2/21.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet var symbolLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    func update(with emoji: Emoji){
+        symbolLabel.text = emoji.symbol
+        nameLabel.text = emoji.name
+        descriptionLabel.text = emoji.description
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
